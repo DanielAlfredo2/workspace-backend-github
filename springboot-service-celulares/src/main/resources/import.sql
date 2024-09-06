067cab9d3374c1a901dec745b92474fc81d33d61
DanielAlfredo2 <daspnueve@gmail.com>
2024-09-05 19:38:33 -0600

    Introducción a Microservicios Spring Boot 1/2

---
+++ springboot-service-celulares/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO celulares (name, marca, created_at) VALUES ('Samsung_danielSegura', 'S21', NOW());
+INSERT INTO celulares (name, marca, created_at) VALUES ('Samsung_danielSegura', 'S22', NOW());
+INSERT INTO celulares (name, marca, created_at) VALUES ('Samsung_danielSegura', 'S23', NOW());
+INSERT INTO celulares (name, marca, created_at) VALUES ('Apple_danielSegura', 'Iphone 15', NOW());
+INSERT INTO celulares (name, marca, created_at) VALUES ('Apple_danielSegura', 'Iphone 15', NOW());
+INSERT INTO celulares (name, marca, created_at) VALUES ('Xiaomi_danielSegura', 'Mi', NOW());
